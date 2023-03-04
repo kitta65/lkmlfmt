5242b77a3743cb53506893bddccb8538927fd0f4
dr666m1 <skndr666m1@gmail.com>
2023-03-04 11:20:21 +0900

    fix fmt and improve test

--- Makefile
+++ Makefile
@@ -3,3 +3,7 @@ test:
 	poetry run flake8
 	poetry run mypy ./**/*.py --strict
 	poetry run pytest
+
+.PHONY: fmt
+fmt:
+	poetry run black .
--- tests/test_lktk.py
+++ tests/test_lktk.py
@@ -1,9 +1,20 @@
 from lktk.parser import lkml_parser
+import pytest
 
-def test_dummy() -> None:
-    tree = lkml_parser.parse('project_name: "my project name"')
-    assert tree.pretty() == """lkml
+
+@pytest.mark.parametrize(
+    "input_,output",
+    [
+        (
+            'project_name: "my project name"',
+            """lkml
   pair
     project_name
     "my project name"
-"""
+""",
+        ),
+    ],
+)
+def test_dummy(input_: str, output: str) -> None:
+    tree = lkml_parser.parse(input_)
+    assert tree.pretty() == output
