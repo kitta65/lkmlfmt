8d7ad8435e7db222968b6936e25a49483bc82f7e
dr666m1 <skndr666m1@gmail.com>
2023-03-04 21:40:32 +0900

    fix cli

--- lktk/command.py
+++ lktk/command.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from pathlib import Path
 import argparse
 
 from lktk.parser import lkml_parser
@@ -6,14 +7,14 @@ from lktk.parser import lkml_parser
 
 @dataclass
 class Args:
-    filepath: str
+    filepath: Path
 
 
 def parse_args() -> Args:
     parser = argparse.ArgumentParser(
         prog="Looker Toolkit", description="unofficial looker CLI"
     )
-    parser.add_argument("filepath")
+    parser.add_argument("filepath", type=Path)
     args = parser.parse_args()
 
     return Args(filepath=args.filepath)
@@ -21,5 +22,6 @@ def parse_args() -> Args:
 
 def run() -> None:
     args = parse_args()
-    tree = lkml_parser.parse(args.filepath)
+    with open(args.filepath) as f:
+        tree = lkml_parser.parse(f.read())
     tree.pretty()
