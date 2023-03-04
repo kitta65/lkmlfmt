7e48b69a6f86672d79aa183a80cbb62184b2f900
dr666m1 <skndr666m1@gmail.com>
2023-03-04 22:55:13 +0900

    use sub command

--- lktk/command.py
+++ lktk/command.py
@@ -7,6 +7,7 @@ from lktk.parser import lkml_parser
 
 @dataclass
 class Args:
+    subcmd: str
     filepath: Path
 
 
@@ -14,14 +15,29 @@ def parse_args() -> Args:
     parser = argparse.ArgumentParser(
         prog="Looker Toolkit", description="unofficial looker CLI"
     )
+    parser.add_argument("subcmd", type=Path)
     parser.add_argument("filepath", type=Path)
     args = parser.parse_args()
 
-    return Args(filepath=args.filepath)
+    return Args(
+        subcmd=args.subcmd,
+        filepath=args.filepath,
+    )
+
+
+def debug(args: Args) -> None:
+    with open(args.filepath) as f:
+        tree = lkml_parser.parse(f.read())
+    print(tree.pretty())
 
 
 def run() -> None:
     args = parse_args()
-    with open(args.filepath) as f:
-        tree = lkml_parser.parse(f.read())
-    print(tree.pretty())
+
+    match args.subcmd:
+        case "debug":
+            debug(args)
+        case "fmt":
+            pass
+        case _:
+            print(f"invalid subcmd: {args.subcmd}")
