a94a9224c881ebbada1edc645a90d7d163ccd494
dr666m1 <skndr666m1@gmail.com>
2023-03-18 12:08:32 +0900

    define ParentSetter

--- lktk/parser.py
+++ lktk/parser.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from lark import Lark, ParseTree, Token
+from lark import Lark, ParseTree, Token, Visitor
 
 DIR = Path(__file__).parent
 
@@ -17,9 +17,20 @@ with open(DIR / "lkml.lark") as f:
     )
 
 
+# https://lark-parser.readthedocs.io/en/latest/recipes.html#keeping-track-of-parents-when-visiting
+class ParentSetter(Visitor[Token]):  # Visitor[Token] means Visitor of ParseTree
+    def __default__(self, tree: ParseTree) -> None:
+        for child in tree.children:
+            if isinstance(child, Token):
+                continue
+            child._parent = tree  # type: ignore
+
+
 # NOTE don't execute this function asynchronously
-def parse(path: Path) -> tuple[ParseTree, list[Token]]:
+def parse(path: Path, set_parent: bool = False) -> tuple[ParseTree, list[Token]]:
     comments.clear()
     with open(path) as f:
         tree = lkml_parser.parse(f.read())
+    if set_parent:
+        ParentSetter().visit(tree)
     return tree, comments
