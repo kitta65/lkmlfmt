9122c192dad69cb8265abbd9e1fb439a7c080e7b
dr666m1 <skndr666m1@gmail.com>
2023-03-25 16:19:14 +0900

    now, lkml format works

--- lktk/command.py
+++ lktk/command.py
@@ -1,9 +1,14 @@
+import difflib
 import logging
-
-# from dataclasses import dataclass
+import sys
+from collections.abc import Iterable
 from pathlib import Path
 
 import click
+
+from lktk.formatter import LkmlFormatter
+from lktk.logger import logger
+from lktk.parser import parse
 
 # from lktk.formatter import LkmlFormatter
 # from lktk.parser import parse
@@ -15,7 +20,7 @@ import click
     type=click.Choice(
         ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], case_sensitive=False
     ),
-    default="DEBUG",
+    default="WARNING",
 )
 def run(log_level: str) -> None:
     level = getattr(logging, log_level)
@@ -26,14 +31,68 @@ def run(log_level: str) -> None:
 @click.command()
 @click.option("--check", is_flag=True, help="TODO add help message")
 @click.option("--diff", is_flag=True, help="TODO add help message")
-@click.argument("files", type=click.Path(exists=True), nargs=-1)
+@click.argument("files", type=click.Path(exists=True, path_type=Path), nargs=-1)
 def format(check: bool, diff: bool, files: list[Path]) -> None:
-    if check:
-        click.echo("check")
-    if diff:
-        click.echo("diff")
-    for f in files:
-        click.echo(click.format_filename(f))
+    modified: list[bool] = []
+
+    for f in filter_lkml(files):
+        logger.debug(f"formatting {f}")
+        before = f.read_text()
+        tree, comment = parse(before, set_parent=True)
+        after = LkmlFormatter(tree, comment).fmt()
+
+        if before != after:
+            click.echo(f"{f} is modified")
+            modified.append(True)
+        else:
+            click.echo(f"{f} is skipped")
+            modified.append(False)
+
+        if diff:
+            print_diff(before, after)
+
+        if not check:
+            with open(f, "w") as f:
+                f.write(after)
+
+    n_modified = sum(modified)
+    n_skipped = len(modified) - n_modified
+    click.echo(f"{n_modified} files are modified, {n_skipped} files are skipped.")
+
+    if n_modified > 0 and check:
+        sys.exit(1)
 
 
 run.add_command(format)
+
+
+# TODO ignore files listed in .gitignore
+def filter_lkml(files: Iterable[Path]) -> Iterable[Path]:
+    res = []
+
+    for f in files:
+        if f.is_dir():
+            for child in filter_lkml(f.iterdir()):
+                res.append(child)
+        elif f.suffix == ".lkml":  # foo.view.lkml is ok
+            res.append(f)
+
+    return res
+
+
+def print_diff(a: str, b: str) -> None:
+    diffs = difflib.unified_diff(a.splitlines(), b.splitlines())
+
+    # skip --- filename +++ filename
+    for _ in range(2):
+        next(diffs)
+
+    for d in diffs:
+        fg = None
+        if len(d) == 0:
+            pass
+        elif d[0] == "+":
+            fg = "green"
+        elif d[0] == "-":
+            fg = "red"
+        click.echo(click.style(d, fg=fg))
